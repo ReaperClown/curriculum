0d18b556a529fb0374d582010ec3d5118b191314
ReaperClown <the.willian.mesquita@gmail.com>
2021-07-07 20:59:23 -0300

    updated css and added more infos

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,10 +1,16 @@
-$(document).ready(function() {
-  $(".toggleDark").click(function() {
+$(document).ready(function () {
+  $(".toggleDark").click(function () {
     $(".background").toggleClass("active");
     $(".contact-info-description").toggleClass("active");
     $("main section").toggleClass("active");
     $("main section p").toggleClass("active");
     $(".social").toggleClass("active");
-    $("section").css("border-bottom", "1px solid #d9e2ee");
+
+    if ($(this).is(":checked"))
+      $("section").css("border-bottom", "1px solid #d9e2ee"),
+      $(".contact-info-image img").css("box-shadow", "rgba(255, 255, 255, 0.2) 0px 20px 30px");
+    else
+      $("section").css("border-bottom", "1px solid #171718"),
+      $(".contact-info-image img").css("box-shadow", "rgba(0, 0, 0, 0.9) 0px 20px 30px");
   });
 });
