93e81c2eb9a2aa72fcd0296d1c1e287432724cc4
ReaperClown <the.willian.mesquita@gmail.com>
2021-07-08 20:04:38 -0300

    Updated multiple sections

--- assets/script/script.js
+++ assets/script/script.js
@@ -7,10 +7,10 @@ $(document).ready(function () {
     $(".social").toggleClass("active");
 
     if ($(this).is(":checked"))
-      $("section").css("border-bottom", "1px solid #d9e2ee"),
+      $("section").css("box-shadow", "0px 24px 3px -24px #d9e2ee"),
       $(".contact-info-image img").css("box-shadow", "rgba(255, 255, 255, 0.2) 0px 20px 30px");
     else
-      $("section").css("border-bottom", "1px solid #171718"),
+      $("section").css("box-shadow", "0px 24px 3px -24px #171718"),
       $(".contact-info-image img").css("box-shadow", "rgba(0, 0, 0, 0.9) 0px 20px 30px");
   });
 });
